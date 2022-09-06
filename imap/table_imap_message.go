9338a9cf748478fc5915b3666e290bef4de0188e
misraved <ved@turbot.com>
2022-09-07 03:17:22 +0530

    Recompile plugin with steampipe-plugin-sdk v4.1.6 and Go version 1.19

--- imap/table_imap_message.go
+++ imap/table_imap_message.go
@@ -10,9 +10,9 @@ import (
 	"github.com/emersion/go-imap"
 	"github.com/jhillyerd/enmime"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
 )
 
 func tableIMAPMessage(ctx context.Context) *plugin.Table {
